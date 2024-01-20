bcacdcefb03c2bea0aef85f3923641b70faac64e
AlgosStile <oleg.todor@mail.ru>
2024-01-20 12:59:32 +0300

    added lombok + get & set

---
+++ demo/src/main/java/com/example/demo/config/SqlConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+public class SqlConfig {
+
+    @Value("${sql.user.findAll}")
+    private String findAllSql;
+
+    @Value("${sql.user.save}")
+    private String saveSql;
+
+    @Value("${sql.user.deleteById}")
+    private String deleteSql;
+
+    @Value("${sql.user.findById}")
+    private String findByIdSql;
+
+    @Value("${sql.user.update}")
+    private String updateSql;
+
+}
